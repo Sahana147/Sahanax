7c8b4296df57fc592bb99b2d276f50b5c55114df
Sahana147 <sahana.xworkz@gmail.com>
2020-12-05 22:17:21 +0530

    adding music file

---
+++ music-java/src/com/xworkz/Music.java
@@ -0,0 +1,16 @@
+package com.xworkz;
+
+public class Music {
+	
+	String name;// static variable
+	String categories;
+	String musicDirector;
+	
+	void relax()
+	{
+	
+		System.out.println("relaxation");
+		
+	}
+	
+}
