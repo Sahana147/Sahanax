88ef60fc771d65af6828305f123417cec0f7aba7
Sahana147 <sahana.xworkz@gmail.com>
2020-12-08 10:41:02 +0530

    adding file

---
+++ dress/src/com/xworkz/dress/Dress.java
@@ -0,0 +1,39 @@
+package com.xworkz.dress;
+
+public class Dress {
+	
+	private String name;
+	private String color;
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name =name;
+		
+	}
+	
+	public String getcolor() {
+		return color;
+	}
+	
+	
+	public void setColor(String color) {
+		this.color = color;
+		}
+	
+	
+	}
+
+
+
+	
+
+
+
+	
+
+
---
+++ dress/src/com/xworkz/util/DressUtil.java
@@ -0,0 +1,14 @@
+package com.xworkz.util;
+
+import com.xworkz.dress.Dress;
+
+public class DressUtil {
+	public static void main(String[] args) {
+		Dress d = new Dress();
+		d.setColor("light green");
+		d.setName("gown");
+		System.out.println(d.getcolor() + "\n" + d.getName());
+		
+	}
+
+}
