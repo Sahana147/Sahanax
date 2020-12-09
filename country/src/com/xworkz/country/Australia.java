868ea91288ac5543fb4ee4c7637bdc7534f4cf91
Sahana147 <sahana.xworkz@gmail.com>
2020-12-09 22:10:24 +0530

    adding inheritance project

---
+++ country/src/com/xworkz/country/Australia.java
@@ -0,0 +1,9 @@
+package com.xworkz.country;
+
+public class Australia extends Country{
+	
+	public Australia(int noOfStates, int noOfLanguages, String capital) {
+		super(noOfStates, noOfLanguages,capital);
+	}
+
+}
---
+++ country/src/com/xworkz/country/Country.java
@@ -0,0 +1,24 @@
+package com.xworkz.country;
+
+public class Country {
+	 public int noOfStates;
+	 public int noOfLanguages;
+	  public String capital;
+	 
+	 public Country(int noOfStates, int noOfLanguages, String capital) {
+		 this.noOfStates = noOfStates;
+		 this.noOfLanguages = noOfLanguages;
+		 this.capital = capital;
+	 }
+		
+		 
+	public void culture() 
+	{
+		System.out.println("we can find differnt identity for differnt countries");
+	}
+		 
+	 }
+	 
+	 
+
+
