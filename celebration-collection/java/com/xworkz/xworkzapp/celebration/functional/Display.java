714c5f2dd6151049acfc2e3585e93892ea37f54a
Sahana147 <sahana.xworkz@gmail.com>
2021-02-11 02:28:46 +0530

    added

--- celebration-collection/java/com/xworkz/xworkzapp/celebration/functional/Display.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.xworkzapp.celebration.functional;
-
-@FunctionalInterface // A class which has only one abstract method is called functionalInterface
-public interface Display {
-
-	void print(String str, String str1);
-
-}
--- celebration-collection/java/com/xworkz/xworkzapp/celebration/service/DisplayService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.xworkzapp.celebration.service;
-
-import com.xworkz.xworkzapp.celebration.functional.Display;
-
-public interface DisplayService {
-
-	void printString(Display display);
-
-}
